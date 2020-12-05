cc6173a0f1cbd43fef5f29d75c409e9f658fe8bf
Isti <steven.trautmann1@gmail.com>
2020-12-05 20:28:15 +0100

    basic events, communication between components

---
+++ src/components/ThoughtItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="thought" @click="favouriteThought(thought.id)">
+        <p>the thought's owner: {{user}}</p>
+        <p v-if="thought.time">thought's time: {{thought.time}}</p>
+        <p v-else>this thought's does not have time</p>
+        <p>{{thought.content}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ThoughtItem",
+    props: {
+        thought: {type: Object, required: true},
+        user: {type: String, required: true}
+    },
+    methods: {
+        favouriteThought(id){
+            this.$emit("favourite", id);
+        }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.thought{
+    background: $thoughtColor;
+    margin: 2rem;
+}
+</style>
--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,25 +1,58 @@
 <template>
   <div class="container">
     <p>{{ user.userName }} -is- {{ fullname }}</p>
+    <p v-if="user.isAdmin">Admin</p>
+    <p v-else-if="followers > 10">Not Admin, but has more than 10 followers</p>
+    <p v-else>Not Admin</p>
     <strong>Followers: {{followers}}</strong>
     <button @click="increaseFollowers">Increase Followers</button>
     <button @click="decreaseFollowers">Decrease Followers</button>
   </div>
+  <div class="thoughts-container">
+      <h1>Thoughts</h1>
+      <ThoughtItem v-for="(thought, index) in user.thoughts" 
+                    :key="index" :thought="thought" 
+                    :user="user.userName" @favourite="toggleFavourite" />
+  </div>
+  <div>
+    <form style="display: flex; flex-direction: column; margin: 5rem" 
+    @submit.prevent="saveNewThought">
+      <label for="newThought">new thought: ({{newThoughtContendCount}}:180)</label>
+      <textarea name="newThought" rows="4" v-model="newThoughtContent" :class="{'exceeded': newThoughtContendCount > 180}"></textarea>
+      <label for="types">Types:</label>
+      <select name="types" v-model="selectedThoughtType" >
+        <option :value="option.value" v-for="(option, index) in thoughtTypes" :key="index">
+          {{option.name}}
+        </option>
+      </select>
+      <button>Save Thought</button>
+    </form>
+  </div>
 </template>
 
 <script>
+import ThoughtItem from "./ThoughtItem"
+
 export default {
     name: "UserProfile",
     data(){
     return {
+      thoughtTypes: [{value: "draft", name: "Draft"}, {value: "suicidal", name: "Suicidal"}],
       followers: 0,
+      newThoughtContent: "",
+      selectedThoughtType: "draft",
       user: {
         id: 1,
         userName: "BMarge",
         firstName: "Margharet",
         lastName: "Black",
         email: "test@gmail.com",
-        isAdmin: true
+        isAdmin: true,
+        thoughts: [
+            { id: 2, time: new Date("2018.07.03.").toDateString(), content: "sooo....much...fun......."},
+            { id: 1, time: new Date("11. 03. 2017").toDateString(), content: "hmm.. yea, living is fun"},
+            { id: 0, time: null, content: "why not just die"},
+        ]
       }
     }
   },
@@ -31,22 +64,39 @@ export default {
     }
   },
   computed: {
-    fullname(){return `${this.user.firstName} ${this.user.lastName}`}
+    fullname(){return `${this.user.firstName} ${this.user.lastName}`},
+    newThoughtContendCount(){return this.newThoughtContent.length}
   },
   methods: {
     increaseFollowers(){this.followers++},
     decreaseFollowers(){this.followers--},
+    toggleFavourite(id){
+      alert(`favourited thought's number: ${id}`)
+    },
+    saveNewThought(){
+      if (this.newThoughtContent !== "" && this.selectedThoughtType !== "draft"){
+        this.user.thoughts.unshift({id: this.user.thoughts[0].id+1, time: new Date().toDateString(), content: this.newThoughtContent });
+        this.newThoughtContent = "";
+      }
+    }
   },
   mounted() {
     return this.increaseFollowers();
+  },
+  components: {
+    ThoughtItem
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
     .container {
     display: flex;
     justify-content: space-around;
     align-items: center;
     }
+
+    .exceeded {
+        border: solid 0.2rem red;
+      }
 </style>
